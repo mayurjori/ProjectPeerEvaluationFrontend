3136cba009fc5e561383a50f5c59a8c9f33359e7
MayurJori <mbjori@gmail.com>
2020-06-12 00:09:16 -0400

    Add sample login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'PeerEvaluationFrontEnd';
+  username = '';
+  password ='';
+
+  signIn(){
+    console.log("Clicked");
+  }
 }
